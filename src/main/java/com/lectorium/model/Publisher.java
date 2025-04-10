15f02cf9a023d1f24aff24c3bb7bb25c46fb2ea3
2004Style <rubencithochavez036@gmail.com>
2025-04-10 11:42:22 -0500

    Actualizacion de cambios

--- src/main/java/com/lectorium/model/Publisher.java
+++ src/main/java/com/lectorium/model/Publisher.java
@@ -1,5 +1,7 @@
 package com.lectorium.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,9 @@ import lombok.NoArgsConstructor;
 @Data // representa a los getters y setters
 @NoArgsConstructor // representa a los constructores sin argumentos
 @AllArgsConstructor // representa a los constructores con argumentos
-
+@Entity
 public class Publisher {
+    @Id
     private Integer idPublisher;
     private String name;
 }
